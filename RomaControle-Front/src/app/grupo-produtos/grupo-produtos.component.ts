1d81ea125de8f0c3d0febe8c3c2f340b300633b0
petinari <petinari@outlook.com>
2023-04-22 12:51:26 -0400

    22042023

--- RomaControle-Front/src/app/grupo-produtos/grupo-produtos.component.ts
+++ RomaControle-Front/src/app/grupo-produtos/grupo-produtos.component.ts
@@ -1,47 +1,45 @@
 import { Component, OnInit } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-
-
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-grupo-produtos',
   templateUrl: './grupo-produtos.component.html',
   styleUrls: ['./grupo-produtos.component.scss'],
 })
-
-
-
 export class GrupoProdutosComponent implements OnInit {
+  public grupoProdutos: any = [];
+  private _filtroGrupoProdutos: string = '';
+  grupoProdutosFiltrados: any = [];
 
-  public grupoProdutos: any=[]
-  private _filtroGrupoProdutos: string = ""
-
-  public get filtroGrupoProdutos(): string{
-    return this._filtroGrupoProdutos
+  public get filtroGrupoProdutos(): string {
+    return this._filtroGrupoProdutos;
   }
 
-public set filtroGrupoProdutos(value: string){
-    this._filtroGrupoProdutos = value
-    this.grupoProdutos = this.filtroGrupoProdutos ? this.filtrarGrupoProdutos(this.grupoProdutos) : this.grupoProdutos
-}
-  constructor(private http: HttpClient) {
+  public set filtroGrupoProdutos(value: string) {
+    this._filtroGrupoProdutos = value;
+    this.grupoProdutosFiltrados = this.filtroGrupoProdutos
+      ? this.filtrarGrupoProdutos(this.filtroGrupoProdutos)
+      : this.grupoProdutos;
   }
+  constructor(private http: HttpClient) {}
 
-   ngOnInit() {
-     this.getGrupoProdutos()
+  ngOnInit() {
+    this.getGrupoProdutos();
   }
-   getGrupoProdutos() {
-     this.http.get('http://localhost:8080/api/produtos/grupos').subscribe(
-      resp => {
-        this.grupoProdutos = resp
-      }
-    )
+  getGrupoProdutos() {
+    this.http
+      .get('http://localhost:8080/api/produtos/grupos')
+      .subscribe((resp) => {
+        this.grupoProdutos = resp;
+        this.grupoProdutosFiltrados = this.grupoProdutos;
+      });
   }
 
   private filtrarGrupoProdutos(filtrarPor: string): any {
-    filtrarPor = filtrarPor.toString().toLowerCase()
+    filtrarPor = filtrarPor.toString().toLocaleLowerCase();
     return this.grupoProdutos.filter(
-        (grupo: { nome: string; }) => grupo.nome.toLocaleLowerCase().indexOf(filtrarPor) !== -1
-    )
+      (grupo: { nome: any }) =>
+        grupo.nome.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
   }
 }
---
+++ RomaControle-Front/src/models/GrupoProdutos.ts
@@ -0,0 +1,5 @@
+export interface GrupoProdutos {
+  id: string;
+  nome: string;
+  idTenant: string;
+}
